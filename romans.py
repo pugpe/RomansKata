c926ca24c3b827d15ddfe7567979bb7551a82421
Marcel Caraciolo <caraciol@gmail.com>
2010-09-18 14:30:08 -0300

    criacao

---
+++ romans.py
@@ -0,0 +1,21 @@
+#-*-coding: utf-8 -*-
+
+import unittest
+
+
+
+class RomanTest(unittest.TestCase):
+	
+	def testI(self):
+		pass
+
+	
+#==================================
+
+
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+
