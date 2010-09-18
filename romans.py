c94868452468c499e4867480a9111f1a554c7f0d
Marcel Caraciolo <caraciol@gmail.com>
2010-09-18 16:39:45 -0300

    fase16

--- romans.py
+++ romans.py
@@ -9,17 +9,22 @@ class RomanTest(unittest.TestCase):
 
 	def test0ShouldReturnNothing(self):
 		self.assertEqual('', number2roman(0))
+		
 	def testI(self):
 		self.assertEqual('I',number2roman(1))
 		
 	def testII(self):
 		self.assertEqual('II',number2roman(2))
+		
 	def testIII(self):
 		self.assertEqual('III',number2roman(3))
+		
 	def testIV(self):
 		self.assertEqual('IV', number2roman(4))
+		
 	def testV(self):
 		self.assertEqual('V', number2roman(5))
+		
 	def testX(self):
 		self.assertEqual('X', number2roman(10))
 		
@@ -50,7 +55,10 @@ class RomanTest(unittest.TestCase):
 		self.assertEqual('MCDXLIV', number2roman(1444))	
 		
 	def testMMMCMXCIX(self):
-		self.assertEqual('MMMCMXCIX', number2roman(3999))			
+		self.assertEqual('MMMCMXCIX', number2roman(3999))
+		
+	def testC(self):
+		self.assertEqual('C', number2roman(100))				
 		
 #def test(self):
 #	self.assertEqual('', number2roman())
@@ -63,62 +71,56 @@ class RomanTest(unittest.TestCase):
 
 def number2roman(number):
 	
+	romanos = {
+		1000: 'M',
+		2000: 'MM',
+		3000: 'MMM',
+		100: 'C',
+		200: 'CC',
+		300: 'CCC',
+		400: 'CD',
+		500: 'D',
+		600: 'DC',
+		700: 'DCC',
+		800: 'DCCC',
+		900: 'CM',
+		10: 'X',
+		20: 'XX',
+		30: 'XXX', 
+		40: 'XL', 
+		50: 'L',
+		60: 'LX',
+		70: 'LXX',
+		80: 'LXXX',
+		90: 'XC',
+		1: 'I',
+		2: 'II',
+		3: 'III',
+		4: 'IV',
+		5: 'V',
+		6: 'VI',
+		7: 'VII',
+		8: 'VIII',
+		9: 'IX',
+		0: ''
+	}
+	
 	milhar = number / 1000
+	number = number - milhar * 1000
+	milhar = romanos[milhar * 1000]
 	
-	if milhar:
-		number = number - milhar * 1000
-		milhar = milhar * 'M'
-	else:
-		milhar = ''
-	
+		
 	centena = number / 100
 	number = number - centena * 100
-	if centena == 5:
-		centena = 'D'
-	elif centena == 4:
-		centena = 'CD'
-	elif centena == 9:
-		centena = 'CM'
-	elif centena > 5 and centena < 9:
-		centena = 'D' + (centena - 5) * 'C'
-	elif centena >= 1 and centena < 4:
-		centena = 'C' * centena
-	else:
-		centena = ''
+	centena = romanos[centena * 100]
+	
 		
 	dezena = number / 10	
 	number = number - dezena * 10
-	if dezena == 5:
-		dezena = 'L'
-	elif dezena == 4:
-		dezena = 'XL'
-	elif dezena == 9:
-		dezena = 'XC'
-	elif dezena > 5 and dezena < 9:
-		dezena = 'L' + (dezena - 5) * 'X'
-	elif dezena >= 1 and dezena < 4:
-		dezena = 'X' * dezena
-	else:
-		dezena = ''
-		
-	unidade = number
-	if unidade == 5:
-		unidade = 'V'
-	elif unidade == 4:
-		unidade = 'IV'
-	elif unidade == 9:
-		unidade = 'IX'
-	elif unidade > 5 and unidade < 9:
-		unidade = 'V' + (unidade - 5) * 'I'
-	elif unidade >= 1 and unidade < 4:
-		unidade = 'I' * unidade
-	else:
-		unidade = ''
-
+	dezena = romanos[dezena * 10]
 	
-
-		
-		
+	unidade = romanos[number]
+	
 	
 	final = milhar + centena + dezena + unidade		
 	return final	
