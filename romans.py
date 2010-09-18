2e491249225306934877056c263fa629e23e2628
Marcel Caraciolo <caraciol@gmail.com>
2010-09-18 16:07:05 -0300

    fase14

--- romans.py
+++ romans.py
@@ -27,63 +27,80 @@ class RomanTest(unittest.TestCase):
 		self.assertEqual("XX", number2roman(20))
 	def testXV(self):
 		self.assertEqual('XV', number2roman(15))
+		
+	def testXVI(self):
+		self.assertEqual('XVI', number2roman(16))
+		
+	def testLXIV(self):
+		self.assertEqual('LXIV', number2roman(64))
+		
+	def testMCMXC(self):
+		self.assertEqual('MCMXC', number2roman(1990))
 
 		
 #==================================
 
 
 def number2roman(number):
-	number = str(number)
 	
-	final =""
+	milhar = number / 1000
 	
+	if milhar:
+		number = number - milhar * 1000
+		milhar = milhar * 'M'
+	else:
+		milhar = ''
 	
-	caracter = number[-1]
+	centena = number / 100
+	number = number - centena * 100
+	if centena == 5:
+		centena = 'D'
+	elif centena == 4:
+		centena = 'CD'
+	elif centena == 9:
+		centena = 'CM'
+	elif centena > 5 and centena < 9:
+		centena = 'D' + (centena - 5) * 'C'
+	elif centena >= 1 and centena < 4:
+		centena = 'C' * centena
+	else:
+		centena = ''
+		
+	dezena = number / 10	
+	number = number - dezena * 10
+	if dezena == 5:
+		dezena = 'L'
+	elif dezena == 4:
+		dezena = 'XL'
+	elif dezena == 9:
+		dezena = 'XC'
+	elif dezena > 5 and dezena < 9:
+		dezena = 'L' + (dezena - 5) * 'X'
+	elif dezena >= 1 and dezena < 4:
+		dezena = 'X' * dezena
+	else:
+		dezena = ''
+		
+	unidade = number
+	if unidade == 5:
+		unidade = 'V'
+	elif unidade == 4:
+		unidade = 'IV'
+	elif unidade == 9:
+		unidade = 'IX'
+	elif unidade > 5 and unidade < 9:
+		unidade = 'V' + (unidade - 5) * 'I'
+	elif unidade >= 1 and unidade < 4:
+		unidade = 'I' * unidade
+	else:
+		unidade = ''
+
 	
-	if caracter == '1':
-		 final = "I"
-	elif caracter == "2":
-		final = "II"
-	elif caracter == "3":
-		final ="III"
-	elif caracter == "4":
-		final ="IV"
-	elif caracter == "5":
-		final ="V"
-	elif caracter == "6":
-		final ="VI"
-	elif caracter == "7":
-		final ="VII"
-	elif caracter == "8":
-		final ="VIII"
-	elif caracter == "9":
-		final ="IX"
-	elif caracter == '0':
-		final = ''
-	 	
+
+		
+		
 	
-	if len(number) > 1:
-		caracter = number[-2]
-	
-		if caracter == '1':
-			final = "X" + final
-		elif caracter == "2":
-			final = "XX" + final
-		elif caracter == "3":
-			final = "XX" + final
-		elif caracter == "4":
-			final = "XL" + final
-		elif caracter == "5":
-			final = "L" + final
-		elif caracter == "6":
-			final = "LX" + final
-		elif caracter == "7":
-			final = "LXX" + final
-		elif caracter == "8":
-			final = "LXXX" + final
-		elif caracter == "9":
-			final = "XC" + final
-			
+	final = milhar + centena + dezena + unidade		
 	return final	
 	
 	
